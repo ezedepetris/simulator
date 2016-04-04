0d0646908fb59d51995c897f1ff6377bcbaec7a5
ezedepetris <ezedepetris@gmail.com>
2016-04-04 00:13:29 -0300

    add real timer and connect all structure (compile and run) :+1:

--- simulator.c
+++ simulator.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-float clock;
+float timer;
 int number_server;
 
 typedef struct ev{
@@ -163,11 +163,28 @@ Event del (List* l){
 
 	NodeNE *destroyed;
 
-	if((l->in).in < (aux->info).out)
+	if(l->lenght == 0){
 		aux_info = l->in;
-	else{
+		(l->in).in = -1;
+		return aux_info;
+
+	}
+
+	if((l->in).in == -1){
 		aux_info = aux->info;
 		l->node = (l->node)->next;
+		l->lenght--;
+		free(aux);
+		return  aux_info;
+	}
+
+	if((l->in).in < (aux->info).out && (l->in).in > 0){
+		aux_info = l->in;
+		(l->in).in = -1;
+	}else{
+		aux_info = aux->info;
+		l->node = (l->node)->next;
+		l->lenght--;
 		free(aux);
 	}
 	return  aux_info;
@@ -184,42 +201,33 @@ void show_l (List* l){
 }
 
 int is_empty_l (List* l){
-	return	(l->lenght == 0)? 0 : 1;
+	return	(l->lenght == 0 && (l->in).in == -1)? 0 : 1;
 }
 // End of Method of Lsist
 
-void input (Queue* queue, Event e, List* list){
+void input (Queue* queue, Event e, List* list,float timer){
 	// do all thing when arrive an event (move the event to the server or move the event at queue if the server is full)
 	if (number_server == 0){
 		enqueue(queue, e);
 	}
 	else{
 		number_server--;
-		e.out = clock + e.services;
+		e.out = timer + e.services;
 		insert(list, e, 1);
 	}
 }
 
-void output (Queue* queue, List* list){
+void output (Queue* queue, List* list,float timer){
 	// less 1 the server list if the queue is empty or put the next event of the queue to the server
 	if (is_empty_q(queue) == 1){
 		Event e;
 		e = dequeue(queue);
-		e.out = clock + e.services;
+		e.out = timer + e.services;
 		insert(list, e, 1);
 	}
 	else{
 		number_server++;
 	}
-}
-
-Event get_next_event(Event* pe_aux){
-	/*return minimal time beetwen input event and output event*//*(0,event)*//*(1,event) */
-	// return the minimal time beetwen input event and output event of pe array
-	//(pe_aux[0]>pe_aux[1])? return pe_aux[1] : return pe_aux[0];
-	Event my_record;
-
-	return my_record;
 }
 
 void update(){
@@ -236,7 +244,7 @@ int end_simulation(Queue* q, List* l){
 	return (is_empty_q(q)==0 &&  is_empty_l(l)==0)? 0: 1;
 }
 
-void simulate(){
+void simulate(float timer){
 	// initialize Queue
 	Queue *queue;
 	queue = (Queue*) malloc(sizeof(Queue));
@@ -246,6 +254,9 @@ void simulate(){
 	List *list;
 	list = (List*) malloc(sizeof(List));
 	list->lenght = 0;
+	Event a;
+	a.in = -1;
+	list->in = a;
 
 	// initialize all variables
 	number_server = 3;
@@ -253,25 +264,80 @@ void simulate(){
 	Event e = array_events[i];
 	insert(list, e, 0);
 
-	while(end_simulation(queue, list) == 1){//not end simulation
+	while(end_simulation(queue, list) != 0){//not end simulation
 		e = del(list);
 
-		if (e.out == 0){
-			clock = clock + e.in;
-			input(queue, e, list);
+		if (e.out == -1){
+			printf("Clock = %f      Soy un evento de Entrada\n", timer );
+			timer = timer + e.in;
+			input(queue, e, list,timer);
 			i++;
 			insert(list, array_events[i], 0);
 		}
 		else{
-			clock = e.out;
-			output(queue, list);
+			printf("Clock = %f      Soy un evento de Salida\n", timer );
+			timer = e.out;
+			output(queue, list,timer);
 		}
 	}
 }
 
 
 int main(int argc, char const *argv[]){
+	Event test;
 
-	simulate();
+	test.in = 1;
+	test.out = -1;
+	test.services = 2;
+	array_events[0] = test;
+	
+	test.in = 2;
+	test.out = -1;
+	test.services = 3.4;
+	array_events[1] = test;
+	
+	test.in = 3;
+	test.out = -1;
+	test.services = 1;
+	array_events[2] = test;
+	
+	test.in = 4;
+	test.out = -1;
+	test.services = 2.5;
+	array_events[3] = test;
+	
+	test.in = 5;
+	test.out = -1;
+	test.services = 2;
+	array_events[4] = test;
+	
+	test.in = 6;
+	test.out = -1;
+	test.services = 6;
+	array_events[5] = test;
+	
+	test.in = 7;
+	test.out = -1;
+	test.services = 5.4;
+	array_events[6] = test;
+	
+	test.in = 8;
+	test.out = -1;
+	test.services = 3;
+	array_events[7] = test;
+	
+	test.in = 9;
+	test.out = -1;
+	test.services = 9;
+	array_events[8] = test;
+	
+	test.in = 10;
+	test.out = -1;
+	test.services = 1;
+	array_events[9] = test;
+
+
+	timer = 0;
+	simulate(timer);
 	return 0;
 }
