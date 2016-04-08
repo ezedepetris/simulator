d9ab7a71ae27500e5a3e760f5228edda5e3e922e
ezedepetris <ezedepetris@gmail.com>
2016-04-08 20:56:36 -0300

    add an interactive script

--- script.sh
+++ script.sh
@@ -1,2 +1,20 @@
-gcc -c simulator simulator.c
-./simulator
+#!/bin/bash
+
+gcc -o simulator simulator.c
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [ -e simulator ]; then
+	printf "${GREEN}compilation success the simulator start now${NC}\n"
+	./simulator
+	exit 0
+else
+	printf "${RED}compilation FAILED${NC}\n"
+	exit 1
+fi
+
+if [ -e simulator ]; then
+	rm simulator
+fi
